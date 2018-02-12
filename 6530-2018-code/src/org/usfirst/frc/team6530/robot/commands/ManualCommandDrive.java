939060ddb3ecb8138c2e2b2d0920f03df5b4fe2a
WarmGreycen <slydog602@gmail.com>
2018-02-12 16:56:38 -0700

    WORKING DRIVE CODE_ sort of. need to fix motor directions

--- 6530-2018-code/src/org/usfirst/frc/team6530/robot/commands/ManualCommandDrive.java
+++ 6530-2018-code/src/org/usfirst/frc/team6530/robot/commands/ManualCommandDrive.java
@@ -1,49 +1,53 @@
 package org.usfirst.frc.team6530.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team6530.robot.OI;
 import org.usfirst.frc.team6530.robot.Robot;
-
-
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
- *
+ * An example command.  You can replace me with your own command.
  */
 public class ManualCommandDrive extends Command {
-	
-	
-	
-	double lastLeftSpeed;
-	double lastRightSpeed;
-	
-    public ManualCommandDrive() {
-    	//sets requirement system
-        requires(Robot.SUB_DRIVE);
-    }
+	public ManualCommandDrive() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.SUB_DRIVE);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		Robot.SUB_DRIVE.setDriveValue(0, 0);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    
-   	
-    	Robot.SUB_DRIVE.tankDrive(OI.DRIVER.getRawAxis(1), OI.DRIVER.getRawAxis(5));
-    }
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.SUB_DRIVE.DriveWithJoystick(OI.DRIVER);
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		exit();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		exit();
+	}
+	
+	private void exit() {
+		Robot.SUB_DRIVE.setDriveValue(0, 0);
+	}
+	
 }
--- 6530-2018-code/src/org/usfirst/frc/team6530/robot/subsystems/subsystemDrive.java
+++ 6530-2018-code/src/org/usfirst/frc/team6530/robot/subsystems/subsystemDrive.java
@@ -1,43 +1,29 @@
-package org.usfirst.frc.team6530.robot.subsystems
-	   /** TalonSRX leftMotor1 = new TalonSRX(Constants.LEFT_SLAVE1);
-	    TalonSRX leftMotor2 = new TalonSRX(Constants.LEFT_SLAVE2);
-	    TalonSRX leftMotor3 = new TalonSRX(Constants.LEFT_MASTER);
-	    TalonSRX rightMotor1 = new TalonSRX(Constants.RIGHT_SLAVE1);
-	    TalonSRX rightMotor2 = new TalonSRX(Constants.RIGHT_SLAVE2);
-	    TalonSRX rightMotor3 = new TalonSRX(Constants.RIGHT_MASTER);
-	    package org.usfirst.frc.team3360.robot.subsystems;
-*/
+package org.usfirst.frc.team6530.robot.subsystems;
+
 import org.usfirst.frc.team6530.robot.Robot;
 import org.usfirst.frc.team6530.robot.Constants;
 import org.usfirst.frc.team6530.robot.OI;
 import org.usfirst.frc.team6530.robot.commands.ManualCommandDrive;
+import org.usfirst.frc.team6530.robot.util.Xbox;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
-
-	    /**
-	     * An example subsystem.  You can replace me with your own Subsystem.
-	     */
 public class subsystemDrive extends Subsystem {
 	    	// Put methods for controlling this subsystem
 	    	// here. Call these from Commands.
-	    	/TalonSRX leftMotor1 = new TalonSRX(Constants.LEFT_SLAVE1);
+	    	TalonSRX leftMotor1 = new TalonSRX(Constants.LEFT_SLAVE1);
 		    TalonSRX leftMotor2 = new TalonSRX(Constants.LEFT_SLAVE2);
 		    TalonSRX leftMotor3 = new TalonSRX(Constants.LEFT_MASTER);
 		    TalonSRX rightMotor1 = new TalonSRX(Constants.RIGHT_SLAVE1);
 		    TalonSRX rightMotor2 = new TalonSRX(Constants.RIGHT_SLAVE2);
 		    TalonSRX rightMotor3 = new TalonSRX(Constants.RIGHT_MASTER);
 		    
-	    	TalonSRX TankDriveR1 = Constants.LEFT_SLAVE1;
-	    	TalonSRX TankDriveR2 = RobotMap.TankDriveR2;
-	    	TalonSRX TankDriveR3 = RobotMap.TankDriveR3;
 	    	
-	    	TalonSRX TankDriveL1 = Constants.LEFT_SLAVE1;
-	    	TalonSRX TankDriveL2 = Constants.LEFT_SLAVE1;
-	    	TalonSRX TankDriveL3 = Constants.LEFT_SLAVE1;
 	    	
 	    	
 	    	public void initDefaultCommand() {
@@ -45,10 +31,10 @@ public class subsystemDrive extends Subsystem {
 	    		setDefaultCommand(new ManualCommandDrive());
 	    	}
 
-	    	public void DriveWithJoystick() {
+	    	public void DriveWithJoystick(Joystick joy) {
 	    		
-	    		double JoystickLeftVal = OI.DRIVER.getLJoystick().getRawAxis(1);
-	    		double JoystickRightVal = OI.DRIVER.getRJoystick().getRawAxis(1);
+	    		double JoystickLeftVal = Xbox.LEFT_Y(joy);
+	    		double JoystickRightVal = Xbox.RIGHT_X(joy);
 	    		
 	    		
 	    		
@@ -61,26 +47,26 @@ public class subsystemDrive extends Subsystem {
 	    		}
 	    		
 	    		
-	    		TankDriveR1.set(ControlMode.PercentOutput, -JoystickRightVal);
-	    		TankDriveR2.set(ControlMode.PercentOutput, -JoystickRightVal);
-	    		TankDriveR3.set(ControlMode.PercentOutput, -JoystickRightVal);
+	    		rightMotor1.set(ControlMode.PercentOutput, -JoystickRightVal);
+	    		rightMotor2.set(ControlMode.PercentOutput, -JoystickRightVal);
+	    		rightMotor3.set(ControlMode.PercentOutput, -JoystickRightVal);
 	    		
-	    		TankDriveL1.set(ControlMode.PercentOutput, JoystickLeftVal);
-	    		TankDriveL2.set(ControlMode.PercentOutput, JoystickLeftVal);
-	    		TankDriveL3.set(ControlMode.PercentOutput, JoystickLeftVal);
+	    		leftMotor1.set(ControlMode.PercentOutput, JoystickLeftVal);
+	    		leftMotor2.set(ControlMode.PercentOutput, JoystickLeftVal);
+	    		leftMotor3.set(ControlMode.PercentOutput, JoystickLeftVal);
 	    		
 	    		
 	    		
 	    	}
 	    	
 	    	public void setDriveValue(double RightVal, double LeftVal) {
-	    		TankDriveR1.set(ControlMode.Position, -RightVal);
-	    		TankDriveR2.set(ControlMode.Position, -RightVal);
-	    		TankDriveR3.set(ControlMode.Position, -RightVal);
+	    		rightMotor1.set(ControlMode.Position, -RightVal);
+	    		rightMotor2.set(ControlMode.Position, -RightVal);
+	    		rightMotor3.set(ControlMode.Position, -RightVal);
 	    		
-	    		TankDriveL1.set(ControlMode.Position, -LeftVal);
-	    		TankDriveL2.set(ControlMode.Position, -LeftVal);
-	    		TankDriveL3.set(ControlMode.Position, -LeftVal);
+	    		leftMotor1.set(ControlMode.Position, -LeftVal);
+	    		leftMotor2.set(ControlMode.Position, -LeftVal);
+	    		leftMotor3.set(ControlMode.Position, -LeftVal);
 	    	}
 
 
